7faf37508fbe4815422183a41af60e223d48e2e7
Ais Wallenstein <83851691+orion2182@users.noreply.github.com>
2024-12-23 04:03:03 +0000

    memanggil docker build dengan parameter

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,9 +1,9 @@
 def call() {
 echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t azeshion21/demo-app:jma-2.0 .'
+        sh "docker build -t $imageName ."
         sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push azeshion21/demo-app:jma-2.0'
+        sh "docker push $imageName"
     }
 }
 
