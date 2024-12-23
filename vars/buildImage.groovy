79c3d731394cba8dbd99f068944ae32e106834b4
Ais Wallenstein <83851691+orion2182@users.noreply.github.com>
2024-12-23 04:51:42 +0000

    docker groovy changes

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 def call(String imageName) {
 echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
@@ -6,4 +7,10 @@ echo "building the docker image..."
         sh "docker push $imageName"
     }
 }
+=======
+import com.example.Docker
+>>>>>>> 157ada3 (docker groovy changes)
 
+deff call(String imageName) {
+      return new Docker(this).buildDockerImage(imageName)
+}
